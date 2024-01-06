00674ab9a8cf6c1407cf1ce060d845b99fd93933
OlgaKV <oliolikarpovabusuu@yandex.ru>
2024-01-07 03:24:06 +0400

    Solution from seminar 1 OOP

--- Seminar1/java/VendingMachine/src/App.java
+++ Seminar1/java/VendingMachine/src/App.java
@@ -17,11 +17,12 @@ public class App {
         Product item3 = new Product(90, 16, "Chips", 1236);
         Product item4 = new Product(20, 18, "Milka", 1237);
         Product item5 = new Product(60, 1, "3korochki", 1238);
-        Product item6 = new Product(150, 2, "snikers", 1239);
-        Product item7 = new Bottle(150, 3, "Cola", 1240, 0.5f);
-        Product item8 = new HotDrink(100, 1, "Cappuccino", 1241, 75.6f);
-       
-        // System.out.println(item8);
+        Product item6 = new Product(60, 2, "snikers", 1239);
+        Product item7 = new Bottle(100, 3, "Cola", 1240, 1.5f);
+        Product item8 = new HotDrink(180, 8, "Cappuccino", 1241, 65.5f);
+        Product item9 = new HotDrink(70, 7, "Black tea", 1241, 100.0f);
+        Product item10 = new HotDrink(70, 6, "Green tea", 1241, 100.0f);
+        Product item11 = new HotDrink(120, 5, "Americano", 1241, 100.0f);
 
         Holder hold = new Holder();
         CoinDispenser coin = new CoinDispenser();
@@ -36,6 +37,9 @@ public class App {
         listProd.add(item6);
         listProd.add(item7);
         listProd.add(item8);
+        listProd.add(item9);
+        listProd.add(item10);
+        listProd.add(item11);
 
         VendingMachine vm = new VendingMachine(hold, coin, disp, listProd);
 
